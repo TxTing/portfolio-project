828ce484e223238c861d56227c2ddef56107bcc2
txting <johnny6night@gmail.com>
2018-07-11 22:29:02 +0800

    create the website form

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,11 @@ class Blog(models.Model):
     pub_date = models.DateTimeField('date published')
     body = models.TextField(max_length=200)
     image = models.ImageField(upload_to='images/')
+
+
+    def __str__(self):
+        return self.title
+    def summary(self):
+        return self.body[:50]
+    def less_pub_date(self):
+        return self.pub_date.strftime('%b %e %Y')
