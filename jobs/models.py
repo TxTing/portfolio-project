1d213c3b96224376fcc463eda9ee58001737829c
txting <johnny6night@gmail.com>
2018-07-15 18:45:31 +0800

    solved the file conflict

--- jobs/models.py
+++ jobs/models.py
@@ -4,3 +4,9 @@ from django.db import models
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
+    pub_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.summary
+    def less_pub_date(self):
+        return self.pub_date.strftime('%b %e %Y')
--- jobs/views.py
+++ jobs/views.py
@@ -5,3 +5,4 @@ from .models import Job
 def home(request):
     jobs = Job.objects.all()
     return render(request, 'jobs/home.html', {'jobs':jobs})
+    
