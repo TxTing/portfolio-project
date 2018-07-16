1479f8c14124bd8d9eb3f8c8aabf4c189fbdb802
txting <johnny6night@gmail.com>
2018-07-16 11:36:56 +0800

    change the db password

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER': 'postgres',
-        'PASSWORD':'cindy6911',
+        'PASSWORD':'kkman1234',
         'HOST':'localhost',
         'PORT':'5432',
     }
